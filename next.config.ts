0f4dcf5f0ae75c792c23fb56bc54e66f0914ee9d
Chaitali7 <chaitalichaudhary25@gmail.com>
2025-03-17 18:46:29 +0530

    fix: build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: '/Personal-Finance-Tracker',
+  assetPrefix: '/Personal-Finance-Tracker/',
   /* config options here */
 };
 
 export default nextConfig;
+module.exports = nextConfig;
+
